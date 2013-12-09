b2ab179f2103be6563f1829854f2b8e6ca007a9e
diego <diego.sanches@gmail.com>
2013-12-09 17:46:01 -0300

    Task supports jobs

--- test/task.js
+++ test/task.js
@@ -1,33 +1,56 @@
-var should = require("should");
-var mock = require('jsmocktool').mock;
-
 var Task = require("../lib/task.js").Task;
 
 describe('Task', function() {
   var task = null;
+  var aFakeJob = { run: function() { /* do nothing on porpouse */ } };
 
-  beforeEach( function() {
-    task = new Task();
-  });
+  describe('run not empty task', function() {
+
+    beforeEach( function() {
+      task = new Task( [ aFakeJob, aFakeJob ] );
+    });
   
-  describe('#run', function() {
-    it('should invoke all callbacks', function() {
-      mock(task).should_receive( "onProgress" ).with_param(100,100);
+    it('should invoke all callbacks if there is any job', function() {
+      mock(task).should_receive( "onProgress" ).with_param(1,2);
+      mock(task).should_receive( "onProgress" ).with_param(2,2);
       mock(task).should_receive( "onAfter" );
       mock(task).should_receive( "onBefore" );
       task.run();
       mock(task).verify_all();
     });
+
+  })
+
+  describe('run empty task', function() {
+
+    beforeEach( function() {
+      task = new Task();
+    });
+  
+    it('should not invoke onProgress', function() {
+      // 
+      // NOT SUPPORTED by jsmocktool
+      //
+      // mock(task).should_not_receive( "onProgress" );
+      mock(task).should_receive( "onAfter" );
+      mock(task).should_receive( "onBefore" );
+      task.run();
+      mock(task).verify_all();
+    });
+
   })
 
   describe("construction", function() {
+
     describe("jobs specified", function() {
+
       beforeEach( function() {
-        var job = { run: function() {} };
-        task = new Task( [ job, job ] );
+        task = new Task( [ aFakeJob, aFakeJob ] );
       });
 
       it( "should invoke progress twice", function() { 
+        mock(task).should_receive( "onAfter" );
+        mock(task).should_receive( "onBefore" );
         mock(task).should_receive( "onProgress" ).with_param(1,2);
         mock(task).should_receive( "onProgress" ).with_param(2,2);
         task.run();
@@ -35,5 +58,6 @@ describe('Task', function() {
       });
 
     });
+
   });
 })
