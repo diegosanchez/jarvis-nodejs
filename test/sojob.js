59e1fe0971366f8e316a2d4c4e267cf01613a93c
diego <diego.sanches@gmail.com>
2013-12-12 11:01:49 -0300

    SOJob added with its unit-tests

--- test/sojob.js
+++ test/sojob.js
@@ -1,4 +1,44 @@
-var Job = require("../lib/sojob.js").Job;
+var fs = require('fs');
+var soexec = require('child_process').exec
 
-describe('Job', function() {
+var SOJob = require("../lib/sojob.js").SOJob;
+
+describe('SOJob', function() {
+
+  describe('job will faild on porpouse', function() {
+
+    beforeEach( function() {
+      job = new SOJob( "mkdir /forbidden" );
+    });
+  
+    it('should invoke onError callbacks', function() {
+      mock(job).expects( "onSucceed" ).never();
+      mock(job).expects( "onError" );
+      job.execute();
+      mock(job).verify();
+    });
+
+  })
+
+  describe('job will execute successfully', function() {
+    var directory = __dirname + "/.testing";
+
+    beforeEach( function() {
+      job = new SOJob( "mkdir -p " + directory);
+    });
+
+    after( function() {
+      if ( fs.existsSync(directory) )
+        fs.rmdirSync( directory );
+    });
+  
+    it('should invoke onError callbacks', function() {
+      mock(job).expects( "onSucceed" );
+      mock(job).expects( "onError" ).never();
+      job.execute();
+      mock(job).verify();
+    });
+
+  })
+
 })
